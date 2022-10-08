cd63d3208886e794692c480c1e68c7e7970a2ec0
viii1 <viiivek1@gmail.com>
2022-10-08 11:59:27 +0530

    Update test.py

--- test.py
+++ test.py
@@ -129,41 +129,41 @@
 #     print("Bug in user input.")
 
 
-try:
-    a = int(input("Tell me one number: "))
-    b = int(input("Tell me another number: "))
-    print("a/b = ", a/b)
-    print("a+b = ", a+b)
-except ValueError:
-    print("Could not convert to a number.")
-except ZeroDivisionError:
-    print("Can't divide by zero")
-except:
-    print("Something went very wrong.")
+# try:
+#     a = int(input("Tell me one number: "))
+#     b = int(input("Tell me another number: "))
+#     print("a/b = ", a/b)
+#     print("a+b = ", a+b)
+# except ValueError:
+#     print("Could not convert to a number.")
+# except ZeroDivisionError:
+#     print("Can't divide by zero")
+# except:
+#     print("Something went very wrong.")
 
 
 
 # ######################################
 # # EXAMPLE: Raising your own exceptions
 # ######################################
-# def get_ratios(L1, L2):
-#     """ Assumes: L1 and L2 are lists of equal length of numbers
-#         Returns: a list containing L1[i]/L2[i] """
-#     ratios = []
-#     for index in range(len(L1)):
-#         try:
-#             ratios.append(L1[index]/L2[index])
-#         except ZeroDivisionError:
-#             ratios.append(float('nan')) #nan = Not a Number
-#         except:
-#             raise ValueError('get_ratios called with bad arg')
-#         else:
-#             print("success")
-#         finally:
-#             print("executed no matter what!")
-#     return ratios
+def get_ratios(L1, L2):
+    """ Assumes: L1 and L2 are lists of equal length of numbers
+        Returns: a list containing L1[i]/L2[i] """
+    ratios = []
+    for index in range(len(L1)):
+        try:
+            ratios.append(L1[index]/L2[index])
+        except ZeroDivisionError:
+            ratios.append(float('nan')) #nan = Not a Number
+        except:
+            raise ValueError('get_ratios called with bad arg')
+        else:
+            print("success")
+        finally:
+            print("executed no matter what!")
+    return ratios
     
-# print(get_ratios([1, 4], [2, 4]))
+print(get_ratios([1, 4], [2, 4]))
 
 
 # #######################################
